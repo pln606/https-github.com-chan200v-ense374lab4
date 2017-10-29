a2bbdb9ee17260c074c4e86bb3311eb8c7b4a57d
Vincent Chan <chan200v@uregina.ca>
2017-10-28 19:56:42 -0600

    Modified ProductCatalogue: Added a search function for getting products in the catalogue. -VC

--- orderAndProducts/ProductCatalogue.java
+++ orderAndProducts/ProductCatalogue.java
@@ -9,4 +9,25 @@ public class ProductCatalogue
     {
         return listOfProducts;
     }
+
+    public Product getProductByName(String name)
+    {
+        Product returnProduct = null;
+        boolean isDone = false;
+        ListIterator<Product> iterator = this.listOfProducts.listIterator();
+        while(iterator.hasNext() && !isDone)
+        {
+            int comparision = name.compareTo(iterator.next().getName());
+            if (comparision == 0)
+            {
+                returnProduct = iterator.next();
+                isDone = true;
+            }
+            else if(comparision > 0)
+            {
+                isDone = false;
+            }
+        }
+        return returnProduct;
+    }
 }
